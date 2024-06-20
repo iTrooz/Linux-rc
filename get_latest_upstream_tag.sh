737bb8ef785f167e7308320718baa2ded672253a
iTrooz <hey@itrooz.fr>
2024-06-20 10:33:11 +0200

    print informative messages to stderr

--- get_latest_upstream_tag.sh
+++ get_latest_upstream_tag.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 latest_tag=$(curl -s https://api.github.com/repos/torvalds/linux/tags | jq -r '.[0].name')
+>&2 echo "Latest tag from the Linux kernel upstream is $latest_tag"
 echo $latest_tag
--- should_build.sh
+++ should_build.sh
@@ -3,11 +3,11 @@
 latest_tag=$(./get_latest_upstream_tag.sh)
 
 if git tag --list | grep -q "$latest_tag"; then
-    echo "Tag $latest_tag exists in the current git repository."
-    echo "You should not build"
+    >&2 echo "Tag $latest_tag exists in the current git repository."
+    >&2 echo "You should not build"
     exit 1
 else
-    echo "Tag $latest_tag does not exist in the current git repository."
-    echo "You should build"
+    >&2 echo "Tag $latest_tag does not exist in the current git repository."
+    >&2 echo "You should build"
     exit 0
 fi
